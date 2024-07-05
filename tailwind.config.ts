593022803e2570720154fe7d4d95a551f773d371
ksande91 <krisande91@gmail.com>
2024-07-05 11:52:50 +0200

    Remove unused config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,6 @@ const config: Config = {
       fontSize: {
         "vw-1": "1vw",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [
