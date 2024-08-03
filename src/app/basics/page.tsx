ac8fee164c393148f98a21352871ce6b5183fce8
ksande91 <krisande91@gmail.com>
2024-08-03 22:25:06 +0200

    Add className to InfoBox

--- src/app/basics/page.tsx
+++ src/app/basics/page.tsx
@@ -1,16 +1,19 @@
 import TheBasics from "@/components/Headings/TheBasics";
 import InfoBox from "@/components/InfoBox";
-import { type ReactElement, type ReactNode } from "react";
+import { type ReactElement } from "react";
 
-export interface BasicsPageProps {
-  children: ReactNode;
-}
-
-export default function BasicsPage(props: BasicsPageProps): ReactElement {
+export default function BasicsPage(): ReactElement {
   return (
     <div>
       <TheBasics />
-      <InfoBox>Test</InfoBox>
+      <InfoBox title="Info" className="mt-8">
+        <div>
+          This is meant to show you the absolute basics of navigating inside
+          vim/neovim. I made some emulators to emulate the nvim editor and some
+          behaviour may be slightly different, but it should help learn the
+          basics.
+        </div>
+      </InfoBox>
     </div>
   );
 }
--- src/components/InfoBox/index.tsx
+++ src/components/InfoBox/index.tsx
@@ -1,9 +1,20 @@
+import { Styleable } from "@/types/stylable";
 import { type ReactElement, type ReactNode } from "react";
 
-export interface InfoBoxProps {
+export interface InfoBoxProps extends Styleable{
+  title: string;
   children: ReactNode;
 }
 
-export default function InfoBox({ children }: InfoBoxProps): ReactElement {
-  return <div>{children}</div>;
+export default function InfoBox({
+  children,
+  title,
+  className = ""
+}: InfoBoxProps): ReactElement {
+  return (
+    <div className={`relative border-solid border-2 border-blue-300 p-8  bg-background ${className}`}>
+      <div className="absolute -top-3 bg-background pl-2 pr-2">{title}</div>
+      {children}
+    </div>
+  );
 }
